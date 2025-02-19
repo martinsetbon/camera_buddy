5b71560c02387924ba7a78a5a48efe07db6882af
Aayush Karki <aayushkarki2001@gmail.com>
2025-02-19 15:49:37 +0900

    added show design

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -3,8 +3,10 @@ import flatpickr from "flatpickr";
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
+  static targets = [ 'startDate' ]
+
   connect() {
-    flatpickr(this.element, {
+    flatpickr(this.startDateTarget, {
       mode: "range",
       dateFormat: "Y-m-d",
       minDate: "today",
